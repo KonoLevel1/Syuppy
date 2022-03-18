192ba2b49449a7745aee7ee899e9807480fc31e9
KonoLevel1 <konolevel1@gmail.com>
2022-03-18 21:57:52 +0900

    アカウント登録機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#top'
+  resource :user, only: [:new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
